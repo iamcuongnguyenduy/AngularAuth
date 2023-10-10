5784d0f74923f3becb0f5801494c0dc36e9cb906
Cuong Nguyen <cuong@mail.com>
2023-10-10 23:07:01 +0700

    test

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -15,11 +16,20 @@ export default class HomeComponent implements OnInit {
   ngOnInit(): void {
     // this.getUser()
   }
-
+  router = inject(Router)
   getUser() {
-    this.authService.getAllUsers().subscribe((res) => {
-      this.users = res.data;
-    });
+    // this.authService.getAllUsers().subscribe((res) => {
+    //   this.users = res.data;
+    // });
     // console.log(this.users);
+    this.authService.getAllUsers().subscribe({
+      next: (res) =>{
+        this.users = res.data
+      }, error: (err) =>{
+        console.log(err);
+        localStorage.removeItem("user_id")
+        this.router.navigate(['login'])        
+      }
+    })
   }
 }
