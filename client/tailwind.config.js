59e7bfa58944991c48ea1e8fbda1a5bb7940bf01
Cuong Nguyen <cuong@mail.com>
2023-11-02 00:21:14 +0700

    q

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}",
+  "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+  // plugins: [require("prettier-plugin-tailwindcss")],
+  
 }
 
