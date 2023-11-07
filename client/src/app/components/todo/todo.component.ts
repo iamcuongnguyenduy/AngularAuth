8527080a14d724330ce1ff28969cbbf5e6b365af
Cuong Nguyen Duy <nduycuong@tma.com.vn>
2023-11-07 21:19:29 +0700

    abc

---
+++ client/src/app/components/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent {
+
+}
